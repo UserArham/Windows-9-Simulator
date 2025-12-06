dc7238518d2e5380418b9ee40e0a0906846e74a7
Arham <snehazahan@yahoo.com>
2025-12-06 13:37:32 -0500

    Create app.svelte

---
+++ app.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { windows } from "./WindowManager.js";
+  import WindowFrame from "./lib/windows/WindowFrame.svelte";
+</script>
+
+{#each $windows as win (win.appId)}
+  <WindowFrame
+    {...win}
+    onClose={() => closeApp(win.appId)}>
+    <svelte:component this={win.component}/>
+  </WindowFrame>
+{/each}
