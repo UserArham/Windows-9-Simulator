899891bdf57451855e9a1b7466b5edbfaec53a05
Arham <snehazahan@yahoo.com>
2025-12-06 13:46:14 -0500

    Create Start menu.svelte

---
+++ Start menu.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { openApp } from "../../WindowManager.js";
+  import apps from "../../config/apps.js";
+</script>
+
+<div class="menu">
+  {#each apps as app}
+    <div class="tile" on:click={() => openApp(app.id, app.component)}>
+      <img src={app.icon}>
+      <span>{app.name}</span>
+    </div>
+  {/each}
+</div>
